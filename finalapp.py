b4adb77653aa2c514731a95f55f134ccbc77621b
SanaKhan1401 <168632891+SanaKhan1401@users.noreply.github.com>
2024-12-30 01:25:44 +0530

    Update finalapp.py

--- finalapp.py
+++ finalapp.py
@@ -34,28 +34,22 @@ with col3:
     pp = st.selectbox("Policy Premium", ['Low Premium', 'Medium Premium', 'Very High Premium', 'Very Low Premium'])
     cg = st.slider("Capital Gains", 0.0, 100000.0, 0.0)
     cl = st.slider("Capital Loss", -100000.0, 100000.0, 0.0)
-
-if st.button("Predict Fraud"):
-   # Map categorical variables
-si_mapping = {'Minor Damage': 0, 'Total Loss': 1, 'Trivial Damage': 2}
-si = si_mapping[si]
-
-oc_mapping = {'Armed Forces': 0, 'Craft RepairExec Managerial': 1, 'Farming Fishing': 2, 'Handlers Cleaners': 3,
+    if st.button("Predict Fraud"):
+        # Map categorical variables
+        si_mapping = {'Minor Damage': 0, 'Total Loss': 1, 'Trivial Damage': 2}
+        si = si_mapping[si]
+        oc_mapping = {'Armed Forces': 0, 'Craft RepairExec Managerial': 1, 'Farming Fishing': 2, 'Handlers Cleaners': 3,
               'Machine Op Inspct': 4, 'Other Service': 5, 'Priv House Serv': 6, 'Prof Specialty': 7,
               'Protective Serv': 8, 'Sales': 9, 'Tech Support': 10, 'Transport Moving': 11}
-oc = oc_mapping[oc]
-
-gr1_mapping = {'Parked Car': 0, 'Single Vehicle Collision': 1, 'Vehicle Theft Incident': 2}
-gr1 = gr1_mapping[gr1]
-
-ct_mapping = {'Front Collision': 0, 'Rear Collision': 1, 'Side Collision': 2}
-ct = ct_mapping[ct]
-
-cp_mapping = {'Fire Authority': 0, 'No Contact': 1, 'Other Contacted': 2, 'Contacted Police': 3}
-cp = cp_mapping[cp]
-
-pp_mapping = {'Low Premium': 0, 'Medium Premium': 1, 'Very High Premium': 2, 'Very Low Premium': 3}
-pp = pp_mapping[pp]
+        oc = oc_mapping[oc]
+        gr1_mapping = {'Parked Car': 0, 'Single Vehicle Collision': 1, 'Vehicle Theft Incident': 2}
+        gr1 = gr1_mapping[gr1]
+        ct_mapping = {'Front Collision': 0, 'Rear Collision': 1, 'Side Collision': 2}
+        ct = ct_mapping[ct]
+        cp_mapping = {'Fire Authority': 0, 'No Contact': 1, 'Other Contacted': 2, 'Contacted Police': 3}
+        cp = cp_mapping[cp]
+        pp_mapping = {'Low Premium': 0, 'Medium Premium': 1, 'Very High Premium': 2, 'Very Low Premium': 3}
+        pp = pp_mapping[pp]
 
 
     input_data = np.array([[ag, wt, ca, si, si1, ih, oc, gr1, ct, cp, pp, cg, cl]])
